363591f11096977a19a657f8b71b736c56451b1c
Rudraksh Kuchiya <rudrakshkuchiya@gmail.com>
2024-04-27 16:22:42 +0530

    Add files via upload

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def euclidean_distance(point1, point2):
+    """
+    Computes euclidean distance of point1 and point2.
+    
+    point1 and point2 are lists.
+    """
+    return np.linalg.norm(np.array(point1) - np.array(point2))
+
+def clusters_distance(cluster1, cluster2):
+    """
+    Computes distance between two clusters.
+    
+    cluster1 and cluster2 are lists of lists of points
+    """
+    return max([euclidean_distance(point1, point2) for point1 in cluster1 for point2 in cluster2])
+  
+def clusters_distance_2(cluster1, cluster2):
+    """
+    Computes distance between two centroids of the two clusters
+    
+    cluster1 and cluster2 are lists of lists of points
+    """
+    cluster1_center = np.average(cluster1, axis=0)
+    cluster2_center = np.average(cluster2, axis=0)
+    return euclidean_distance(cluster1_center, cluster2_center)
